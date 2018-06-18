a4dd8bd03c62842c1119e3be94e05bd85a6edb89
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-06-18 07:33:51 -0700

    added a couple of other server types

--- server-types.go
+++ server-types.go
@@ -1,7 +1,23 @@
 package main
 
-// IntroMessage is the payload that gets sent back on GET /
-type IntroMessage struct {
+import (
+	"net/url"
+)
+
+// RootResponse is the payload that gets sent back on GET /
+type RootResponse struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+}
+
+// GetEchoResponse is the payload to send back on GET /get
+type GetEchoResponse struct {
+	Status      string     `json:"status"`
+	QueryParams url.Values `json:"queryParams"`
+}
+
+// PostEchoResponse is the payload to send back on POST /post
+type PostEchoResponse struct {
+	Status      string     `json:"status"`
+	QueryParams url.Values `json:"queryParams"`
 }
