3ca062a8c74db17caef7b48c8b3895263cf0d3f6
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-09-28 08:43:09 -0700

    added some docs, need some clarification on how this works"

--- main.go
+++ main.go
@@ -13,7 +13,19 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	/**
+	 * @api {GET} / get help about this block
+	 * @apiName Intro
+	 */
 	r.HandleFunc("/", Intro)
+	/**
+	 * @api {GET} /get echoes back what you send it in the query params
+	 * @apiName GetEcho
+	 * @apiParam (QueryString) {String} key1 value of key1
+	 * @apiParam (QueryString) {String} [key2] value of key2
+	 * @apiSuccess (200)
+	 * @apiError (400)
+	 */
 	r.HandleFunc("/get", GetEcho).Methods("GET")
 	r.HandleFunc("/post", PostEcho).Methods("POST")
 
