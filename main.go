15252ab4215b80280f90353672493e6d0a245c6d
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2018-10-07 09:10:57 -0700

    added some documentation, gonna go build now

--- main.go
+++ main.go
@@ -14,19 +14,27 @@ func main() {
 	r := mux.NewRouter()
 
 	/**
-	 * @api {GET} / get help about this block
-	 * @apiName Intro
+	* @api {GET} / get help about this block
+	* @apiName Intro
 	 */
 	r.HandleFunc("/", Intro)
 	/**
-	 * @api {GET} /get echoes back what you send it in the query params
-	 * @apiName GetEcho
-	 * @apiParam (QueryString) {String} key1 value of key1
-	 * @apiParam (QueryString) {String} [key2] value of key2
-	 * @apiSuccess (200)
-	 * @apiError (400)
+	* @api {GET} /get echoes back what you send it in the query params
+	* @apiName GetEcho
+	* @apiParam (QueryString) {String} key1 value of key1
+	* @apiParam (QueryString) {String} [key2] value of key2
+	* @apiSuccess {Object} data the query params encoded as a JSON object
+	* @apiError {String} error.message the error message
 	 */
 	r.HandleFunc("/get", GetEcho).Methods("GET")
+	/**
+	* @api {POST} /post echoes back what you send it in the request body
+	* @apiName PostEcho
+	* @apiParam (Body) {String} key1 value of key1
+	* @apiParam (Body) {String} [key2] value of key2
+	* @apiSuccess {Object} data the request body encoded as a JSON object
+	* @apiError {String} error.message the error message
+	 */
 	r.HandleFunc("/post", PostEcho).Methods("POST")
 
 	loggingRouter := handlers.LoggingHandler(os.Stdout, r)
